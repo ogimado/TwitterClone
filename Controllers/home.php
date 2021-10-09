9d29a46a2723cc054c575e79af2074f0d738efed
ogimado <ogimado1112@gmail.com>
2021-10-09 21:19:00 +0900

    ホーム機能

--- Controllers/home.php
+++ Controllers/home.php
@@ -6,12 +6,14 @@
 include_once '../config.php';
 // 便利な関数読み込み
 include_once '../util.php';
-
+// ツイートデータ操作モデル（Model tweets）を読み込む
+include_once '../Models/tweets.php';
 
 // TODO:ログインチェック
 $user = getUserSession();
+
+// 「値がない＝ログインしていない」➡ログイン画面に遷移
 if(!$user){
-  // 「値がない＝ログインしていない」➡ログイン画面に遷移
   header('Location:'.HOME_URL.'Controllers/sign-in.php');
   exit;
 } 
@@ -21,29 +23,7 @@ $view_user = $user;
 
 //////// ツイート一覧：データ ////////
 // TO DO：モデルから取得
-$view_tweets = [
-  [ 'user_id' => 1,
-    'user_name' => 'taro',
-    'user_nickname' => '太郎',
-    'user_image_name' => 'sample-person.jpg',
-    'tweet_body' => '入力内容<script>alert("スクリプトタグ使えちゃった？")</script>',
-    'tweet_image_name' => null,
-    'tweet_created_at' => '2021-09-01 19:00:00',
-    'like_id' => null,
-    'like_count' => 0
-],
-  [ 'user_id' => 2,
-  
-    'user_name' => 'jiro',
-    'user_nickname' => '次郎',
-    'user_image_name' => null,
-    'tweet_body' => 'コワーキングスペースをオープンしました！',
-    'tweet_image_name' => 'sample-post.jpg',
-    'tweet_created_at' => '2021-03-14 14:00:00',
-    'like_id' => 1,
-    'like_count' => 1
-    ]
-];
+$view_tweets = findTweets($user);
 
 // 画面表示
 include_once '../Views/home.php';
--- Models/tweets.php
+++ Models/tweets.php
@@ -41,3 +41,67 @@ function createTweet(array $data)
 
   return $response;
 }
+
+
+/**
+ * ツイート一覧を取得
+ * 
+ * @param array $user ログインしているユーザー情報
+ * @return array|false
+ */
+
+function findTweets(array $user){
+  $mysqli = new mysqli(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
+  if($mysqli -> connect_errno){
+    echo 'Mysqlの接続に失敗しました。'. $mysqli -> connect_error ."\n";
+    exit;
+  }
+
+  // ログインユーザーIDをエスケープ
+  $login_user_id = $mysqli -> real_escape_string($user['id']);
+
+  // 検索のSQLクエリを作成
+  $query =<<<SQL
+    SELECT 
+      T.id AS tweet_id,
+      T.status AS tewwt_status,
+      T.body AS tweet_body,
+      T.image_name AS tweet_image_name,
+      T.created_at AS tweet_created_at,
+      U.id AS user_id,
+      U.name AS user_name,
+      U.nickname AS user_nickname,
+      U.image_name AS user_image_name,
+      -- ログインユーザーがいいね！したか（いいね！いている場合、値が入る）
+      L.id AS like_id,
+      -- いいね！数
+      -- サブクエリ：外側に紐づく値がある、サブクエリ＝相関サブクエリ  
+      (SELECT COUNT(*) FROM likes WHERE status = 'active' AND tweet_id = T.id ) AS like_count
+
+    FROM
+      tweets AS T
+      -- ユーザーテーブルをuser_idとtweets.user_idで紐付ける
+      JOIN
+      users AS U ON U.id = T.user_id AND U.status = 'active'
+      -- いいね！テーブルをlikes.tweet_idとtweets_idで紐付ける
+      LEFT JOIN
+      likes AS L ON L.tweet_id = T.id AND L.status = 'active' AND L.user_id = '$login_user_id'
+
+    WHERE
+      T.status = 'active'
+  SQL;
+
+  // クエリ実行
+  $result = $mysqli -> query($query);
+  if($result){
+    // データを配列で受け取る
+    $response = $result->fetch_all(MYSQLI_ASSOC);
+  }else{
+    $response = false;
+    echo 'エラーメッセージ：'. $mysqli->error. "\n";
+  }
+
+  $mysqli -> close();
+
+  return $response;
+}
